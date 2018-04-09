8071af1897240a2d4153e5fa43d97b208fb90769
claudio <claudio@claudioaltamura.de>
2018-04-09 09:36:48 +0200

    first example

---
+++ src/main/java/de/claudioaltamura/java/junit5/HelloWorld.java
@@ -0,0 +1,8 @@
+package de.claudioaltamura.java.junit5;
+
+public class HelloWorld {
+
+	public String hello() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/de/claudioaltamura/java/junit5/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package de.claudioaltamura.java.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void testHello() {
+		HelloWorld helloWorld = new HelloWorld();
+		assertEquals("Hello World!", helloWorld.hello());
+	}
+
+}
