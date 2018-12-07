5c23afd3c94dfec0d426ca6d896acc9c148778b3
Claudio Altamura <claudio@claudioaltamura.de>
2018-12-07 13:23:13 +0100

    new: added parameterized test example

---
+++ src/main/java/de/claudioaltamura/java/junit5/Inverter.java
@@ -0,0 +1,21 @@
+package de.claudioaltamura.java.junit5;
+
+public class Inverter {
+
+  private String[] correctWords;
+
+  public Inverter(String... correctWords) {
+    this.correctWords = correctWords;
+  }
+  
+  public boolean isInverted(String invertedWord) {
+    for(String word : correctWords) {
+      String tmp = new StringBuilder(invertedWord).reverse().toString();
+      if(word.equals(tmp)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
---
+++ src/test/java/de/claudioaltamura/java/junit5/ParameterTest.java
@@ -0,0 +1,35 @@
+package de.claudioaltamura.java.junit5;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ParameterTest {
+
+  private Inverter inverter;
+  
+  @BeforeEach
+  void beforeEach() {
+    inverter = new Inverter("Hello", "World");
+  }
+  
+  @ParameterizedTest    
+  @ValueSource(strings = {"olleH", "dlroW"})
+  void isInverted(String input) {
+    assertTrue(inverter.isInverted(input));
+  }
+  
+  @ParameterizedTest    
+  @MethodSource("notInverted")
+  void isNotInverted(String input) {
+      assertFalse(inverter.isInverted(input));
+  }
+
+  static String[] notInverted() {
+      return new String[] {"Hello", "World"};
+  }
+  
+}
