070b150f825a13bc754360ff9e848494523cba27
Claudio Altamura <claudio@claudioaltamura.de>
2019-12-16 09:15:00 +0100

    new: tagged test

---
+++ src/test/java/de/claudioaltamura/java/junit5/TagExample1Test.java
@@ -0,0 +1,19 @@
+package de.claudioaltamura.java.junit5;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("example1")
+class TagExample1Test {
+
+	@Test
+	@DisplayName("test message")
+	void testHello() {
+		Message message = new Message("This is a message!");
+		assertEquals("This is a message!", message.getMessage());
+	}
+}
---
+++ src/test/java/de/claudioaltamura/java/junit5/TagExample2Test.java
@@ -0,0 +1,26 @@
+package de.claudioaltamura.java.junit5;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+class TagExample2Test {
+
+	@Tag("example1")
+	@Test
+	@DisplayName("test hello")
+	void testHello() {
+		HelloWorld helloWorld = new HelloWorld();
+		assertEquals("Hello World!", helloWorld.hello());
+	}
+
+	@Test
+	@Tag("example2")
+	void testDisable() {
+		System.out.println("test is disabled");
+	}
+
+}
