2c3092e97cfe18946531fd84bccaf4c3eb541329
Claudio Altamura <claudio@claudioaltamura.de>
2020-05-05 20:00:01 +0200

    new: Examples for absence and presence of exceptions.

---
+++ src/test/java/de/claudioaltamura/java/junit5/AbsencePresenceTest.java
@@ -0,0 +1,34 @@
+package de.claudioaltamura.java.junit5;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Examples for absence and presence of exceptions.
+ *
+ * <p>see <a href="https://stackoverflow.com/a/51768474">How to test that no exception is thrown?</a>.
+ */
+class AbsencePresenceTest {
+
+  @Test
+  void assertExecutableDoNotThrowExceptions() {
+    assertAll(() -> assertEquals("Hallo", new Message("Hello").getMessage()));
+  }
+
+  @Test
+  void assertExecutableDoesThrowExceptionAnyKind() {
+    assertDoesNotThrow(() -> new Message("Hello").getMessage());
+  }
+
+
+  @Test
+  void assertExecutableThrowsException() {
+    assertThrows(RuntimeException.class, () -> new Inverter().badMethod(42));
+  }
+
+}
