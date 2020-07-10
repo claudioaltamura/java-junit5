dde42576169313e05b4f5c72cfa2fc0785aef340
claudioaltamura <claudio@claudioaltamura.de>
2020-07-10 15:00:07 +0200

    new: added exception tests

---
+++ src/test/java/de/claudioaltamura/java/junit5/ExceptionTest.java
@@ -0,0 +1,31 @@
+package de.claudioaltamura.java.junit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @see <a href="https://stackoverflow.com/a/51768474">test exceptions with JUnit 5</a>
+ */
+class ExceptionTest {
+
+    @Test
+    void assertAllNoExceptions() {
+        Inverter inverter = new Inverter("hello");
+        assertAll(() -> inverter.isInverted("olleh"));
+    }
+
+    @Test
+    void assertDoesNotThrowExceptions() {
+        Inverter inverter = new Inverter("hello");
+        assertDoesNotThrow(() -> inverter.isInverted("elloh"));
+    }
+
+    @Test
+    void assertThrowsException() {
+        Inverter inverter = new Inverter("hello");
+        assertThrows(
+                RuntimeException.class,
+                () -> inverter.badMethod(42));
+    }
+}
