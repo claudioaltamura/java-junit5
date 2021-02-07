e13b5900bb466aaceda5885ffcf827a8f6f346f4
Claudio Altamura <claudio@claudioaltamura.de>
2021-02-07 20:14:33 +0100

    new: parameter test with different types

--- src/test/java/de/claudioaltamura/java/junit5/ParameterTest.java
+++ src/test/java/de/claudioaltamura/java/junit5/ParameterTest.java
@@ -1,9 +1,13 @@
 package de.claudioaltamura.java.junit5;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -32,4 +36,17 @@ class ParameterTest {
     return new String[] {"Hello", "World"};
   }
 
+  private static Stream<Arguments> providedArguments() {
+    return Stream.of(Arguments.of(2, 5, 10, "easy"), Arguments.of(7, 8, 56, "more difficult"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("providedArguments")
+  void multipleArguments(int a, int b, int result, String message) {
+    Calculator calculator = new Calculator();
+
+    System.out.println("message: " + message);
+
+    assertEquals(result, calculator.multiply(a, b));
+  }
 }
