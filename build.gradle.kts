14f8a5bdc3119c6d774d846ba590fe08ddc2ccf8
Claudio Altamura <claudio@claudioaltamura.de>
2021-02-07 20:13:00 +0100

    chore: upgrade

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+    eclipse
+    idea
+    id("com.diffplug.spotless") version "5.9.0"
+    id("com.github.ben-manes.versions") version "0.36.0"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("org.apache.logging.log4j:log4j-api:2.14.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.14.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
+    testImplementation("org.assertj:assertj-core:3.19.0")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+    }
+    kotlinGradle {
+        ktlint()
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        // includeTags 'example1'
+        // excludeTags 'example2'
+        testLogging {
+            events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+        }
+    }
+
+// 	task example2Test(type: Test) {
+// 		useJUnitPlatform {
+// 			includeTags  = "example2"
+// 		}
+// 	}
+}
