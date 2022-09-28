ec583d1215647a494bb156b60fe1cac8559ff218
Claudio Altamura <ich@claudioaltamura.de>
2022-09-28 16:23:26 +0200

    chore: housekeeping

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,19 @@ plugins {
     java
     eclipse
     idea
-    id("com.diffplug.spotless") version "6.5.0"
+    id("com.diffplug.spotless") version "6.11.0"
     id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
 
 spotless {
