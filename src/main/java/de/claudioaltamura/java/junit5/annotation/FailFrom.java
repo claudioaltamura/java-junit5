6d32023d1619326dfec5da1342985fe433fda3bb
Claudio Altamura <claudio@claudioaltamura.de>
2022-09-29 18:22:50 +0200

    new: added api guardiam annotation

--- src/main/java/de/claudioaltamura/java/junit5/annotation/FailFrom.java
+++ src/main/java/de/claudioaltamura/java/junit5/annotation/FailFrom.java
@@ -1,5 +1,6 @@
 package de.claudioaltamura.java.junit5.annotation;
 
+import org.apiguardian.api.API;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.lang.annotation.*;
@@ -8,6 +9,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(FailFromCondition.class)
 @Documented
+@API(status = API.Status.EXPERIMENTAL)
 public @interface FailFrom {
     String date() default "";
     String reason() default "";
