6a636faf0afa26ec8c15256fefad41062ea53fea
claudioaltamura <claudio@claudioaltamura.de>
2025-02-09 23:29:07 +0100

    fix: dynamic test

--- src/test/java/de/claudioaltamura/java/junit5/DynamicExampleTest.java
+++ src/test/java/de/claudioaltamura/java/junit5/DynamicExampleTest.java
@@ -1,21 +1,18 @@
 package de.claudioaltamura.java.junit5;
 
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
-
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
+import java.util.Arrays;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
 class DynamicExampleTest {
 
-
-    @TestFactory
-    Iterable<DynamicTest> test() {
-        return Arrays.asList(
-                dynamicTest("2 * 2", () -> assertEquals(4, 2 * 2)),
-                dynamicTest("3 * 3", () -> assertEquals(9, 3 * 3))
-        );
-    }
+  @TestFactory
+  Iterable<DynamicTest> test() {
+    return Arrays.asList(
+        dynamicTest("2 * 2", () -> assertEquals(4, 2 * 2)),
+        dynamicTest("3 * 3", () -> assertEquals(9, 3 * 3)));
+  }
 }
